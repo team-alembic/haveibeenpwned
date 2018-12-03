bb33cb9c63dcae06caaf1250b8181dd07e568a05
Luke Rollans <hello@lukerollans.me>
2018-12-04 07:38:37 +0800

    Add tests around IO GenServer

---
+++ test/database/io_test.exs
@@ -0,0 +1,36 @@
+defmodule Haveibeenpwned.Database.IOTest do
+  use ExUnit.Case, async: true
+
+  alias Haveibeenpwned.Database.IO
+
+  describe "Haveibeenpwned.Database.IO.start_link/0" do
+    test "starts a named GenServer" do
+      server = Process.whereis(IO)
+      assert Process.alive?(server)
+    end
+  end
+
+  describe "Haveibeenpwned.Database.IO.init/1" do
+    test "sets initial state to the open file handle" do
+      {:ok, pid} = IO.init(:foo)
+      assert Process.alive?(pid)
+    end
+  end
+
+  describe "Haveibeenpwned.Database.IO.handle_call/3" do
+    test "reads and entry of a specific offset and length" do
+      {:ok, bytes} = GenServer.call(IO, {:read_entry, [offset: 10, length: 10]})
+      assert "6957822A1A" == bytes
+    end
+
+    test "reads an entry from a specific offset up to a specific length" do
+      {:ok, bytes} = GenServer.call(IO, {:read_entry, [offset: 135]})
+      assert "34FB3300B9A77BEBDC988EC3EDD0D4A6A42A26F9:522" == bytes
+    end
+
+    test "allows retrieval of the database file process" do
+      pid = GenServer.call(IO, :database_handle)
+      assert Process.alive?(pid)
+    end
+  end
+end
