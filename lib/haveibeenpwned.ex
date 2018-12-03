4f9ba54d0dbdd7dacf0d83567cd8123acc981834
Luke Rollans <hello@lukerollans.me>
2018-12-03 09:06:48 +0800

    Remove some auto-generated stuff

--- lib/haveibeenpwned.ex
+++ lib/haveibeenpwned.ex
@@ -2,17 +2,4 @@ defmodule Haveibeenpwned do
   @moduledoc """
   Documentation for Haveibeenpwned.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Haveibeenpwned.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/haveibeenpwned_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule HaveibeenpwnedTest do
-  use ExUnit.Case
-  doctest Haveibeenpwned
-
-  test "greets the world" do
-    assert Haveibeenpwned.hello() == :world
-  end
-end
