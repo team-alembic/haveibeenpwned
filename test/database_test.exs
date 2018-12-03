fb4ff8490c71cc1e9253d6025e0bc5452d043934
Luke Rollans <hello@lukerollans.me>
2018-12-03 11:40:02 +0800

    Update tests to pass against new fixture

--- test/database_test.exs
+++ test/database_test.exs
@@ -17,12 +17,12 @@ defmodule Haveibeenpwned.Database.Test do
 
   describe "Haveibeenpwned.read_database_portion/2" do
     test "reads the specified portion of the hash database" do
-      assert "0005AD76BD" == Database.read_portion(5, 10)
-      assert ":004" == Database.read_portion(40, 4)
+      assert "A7B8A69578" == Database.read_portion(5, 10)
+      assert ":187" == Database.read_portion(40, 4)
     end
 
     test "returns offset up to EOF if offset extends beyond EOF" do
-      assert "54D43:040\n" == Database.read_portion(440, 100)
+      assert "AC03E:122\n" == Database.read_portion(440, 100)
     end
 
     test "returns an error tuple when offset is at or beyond EOF" do
