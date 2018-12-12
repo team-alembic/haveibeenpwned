510b1fa2653b6f28ded0d11912f25f6c205ef810
Luke Rollans <hello@lukerollans.me>
2018-12-12 15:48:00 +0800

    Start module for downloading text database

---
+++ lib/mix/tasks/hibp/text/download.ex
@@ -0,0 +1,20 @@
+defmodule Mix.Tasks.Hibp.Text.Download do
+  use Mix.Task
+  require Logger
+
+  @moduledoc """
+  Mix task for downloading Troy Hunt's text database file to the local file
+  system
+
+  ## Example usage
+
+  ```
+  mix hibp.text.download
+  ```
+
+  If the download is succesful, the text database file can be found at
+  `priv/hibp_text.txt` relative to your application
+  """
+
+  @download_url "https://downloads.pwnedpasswords.com/passwords/pwned-passwords-ordered-by-hash.7z"
+end
